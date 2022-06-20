982542254f377ac4406a5a19d499c11aa6b65cc1
johnmogi <anguru@gmail.com>
2022-06-20 12:15:59 +0300

    up

--- agenda.md
+++ agenda.md
@@ -1,8 +1,26 @@
 Sela week 6 starts with a bang, trying to recover and upgrade
 ansible_terraform_azure
 
+connect nsg to nic to vm
 
+# connect be nic to nsg
 
+resource "azurerm_network_interface_security_group_association" "be_nsg_assoc" {
+network_interface_id = azurerm_network_interface.network_interface_db[0].id
+network_security_group_id = azurerm_network_security_group.backend_subnet.id
+} # connect fe nic to nsg
+resource "azurerm_network_interface_security_group_association" "fe_nsg_assoc" {
+network_interface_id = azurerm_network_interface.network_interface_app[0].id
+network_security_group_id = azurerm_network_security_group.frontend_subnet.id
+} # connect fe nic to nsg
+resource "azurerm_network_interface_security_group_association" "fe_nsg_assoc1" {
+network_interface_id = azurerm_network_interface.network_interface_app[1].id
+network_security_group_id = azurerm_network_security_group.frontend_subnet.id
+} # connect fe nic to nsg
+resource "azurerm_network_interface_security_group_association" "fe_nsg_assoc2" {
+network_interface_id = azurerm_network_interface.network_interface_app[2].id
+network_security_group_id = azurerm_network_security_group.frontend_subnet.id
+}
 
 <!-- need to build a deploy-
 
@@ -16,3 +34,10 @@ output:
 export password and user
 
 take care that staging is smaller then production as in vm b1s + 2 vm's on stage
+
+providers.tf {connect to azure}
+main.tf { group + locals} // staging prod
+network.tf { create vnet + subnets}
+nsg { create security rules} -> need to connect to nic?
+modules-vm
+loadbalance -> ip + machines + nic mash it all up!
---
+++ nsg.tf
@@ -0,0 +1,58 @@
+ # PRIVATE Network Security Group and rule
+resource "azurerm_network_security_group" "backend_subnet" {
+  name                = "backend_nsg"
+  location            = "${azurerm_resource_group.weight-app.location}"
+  resource_group_name = "${azurerm_resource_group.weight-app.name}"
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "Postgresql"
+    priority                   = 901
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "5432"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+# PUBLIC Network Security Group and rule
+resource "azurerm_network_security_group" "frontend_subnet" {
+  name                = "frontend_nsg"
+  location            = "${azurerm_resource_group.weight-app.location}"
+  resource_group_name = "${azurerm_resource_group.weight-app.name}"
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 901
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
